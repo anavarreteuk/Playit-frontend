605fafc31d2a06625840c327ff9a407d75e4a349
anavarreteuk <a.navarrete.uk@gmail.com>
2018-12-16 23:17:48 +0000

    added form in teachers

--- src/components/Teachers.js
+++ src/components/Teachers.js
@@ -1,23 +1,70 @@
 import React, { Component } from "react";
 import ComplexGrid from './ComplexGrid.js'
 import Map from './Map.js'
+import TextField from "@material-ui/core/TextField";
+
+
+import { withStyles } from "@material-ui/core/styles";
+
+
 
 import "../App.css";
+const styles = theme => ({
+  container: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit
+  },
+  dense: {
+    marginTop: 16
+  },
+  menu: {
+    width: 500
+  }
+});
+
 
 class Teachers extends Component {
+state={
+  inputValue:''
+
+}
+
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value })
+  };
   render() {
-    return <div className="teachersAndMap">
+    const { classes } = this.props;
+    return (
+      <div className="teachersAndMap">
         <div className="teachers">
-          <h1>Teachers by instr</h1>
+          <form className={classes.container} noValidate autoComplete="off">
+          <TextField
+            fullWidth='true'
+            id="outlined-with-placeholder"
+            label="What would you like to learn?"
+            placeholder="Placeholder"
+            className={classes.textField}
+            margin="normal"
+            variant="outlined"
+            onChange={this.handleChange}
+            name='inputValue'
+          />
+          </form>
           <ComplexGrid />
           <ComplexGrid />
-          
         </div>
         <div className="map">
           <Map />
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 
-export default Teachers;
+
+export default withStyles(styles)(Teachers);
