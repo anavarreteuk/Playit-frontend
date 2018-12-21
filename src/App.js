211bd3f91c3f078f179a0a4c050826e16f3e802a
anavarreteuk <a.navarrete.uk@gmail.com>
2018-12-21 11:01:08 +0000

    rendering dynamic routes for calendar working

--- src/App.js
+++ src/App.js
@@ -64,6 +64,7 @@ class App extends Component {
   //   })})
   // }
 
+
   render() {
     return <div className="App">
         <NavBar signout={this.signout} username={this.state.username} />
--- src/components/Availabilities.js
+++ src/components/Availabilities.js
@@ -1,10 +1,38 @@
 import React from 'react'
-
+import API from './API.js'
 export default class Availabilities extends React.Component {
+  constructor() {
+    super();
+
+    var today = new Date(),
+      date = today.getDate()+''+ (today.getMonth() + 1)  +''+ today.getFullYear();
+    console.log(date)
+    this.state = {
+      date: date,
+      availabilities:[]
+    };
+  }
+   
+  
+   
+
+   componentDidMount(){
+     
+     API.availabilities(this.props.match.params.id, this.state.date)
+    .then(data=> this.setState({availabilities:[...data]}))
+  }
+   
+  
   
   render() {
     return (
-      <div><h2>Hello</h2></div>
+      <div><h2>Hello</h2>
+        
+        <h3>{this.state.availabilities.map(a => a.duration)}</h3>
+        <h3>{this.state.availabilities.map(a => a.booked ? 'Available': 'Unavailable')}</h3>
+       </div>
+      
+
     )
   }
 }
