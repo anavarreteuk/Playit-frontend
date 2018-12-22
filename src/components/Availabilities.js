bed74d0f0d87dbe6c22dd03c7d2387c01242ea0b
anavarreteuk <a.navarrete.uk@gmail.com>
2018-12-22 21:12:09 +0100

    buttons next week previous week working

--- src/components/Availabilities.js
+++ src/components/Availabilities.js
@@ -25,11 +25,26 @@ export default class Availabilities extends React.Component {
     return e.substr(0, 7) + "," + e.substr(7);
     }
     
+  todayDays = () => {
+    let result = new Date(this.reverse());
+    result.setDate(result.getDate());
+    let test = result;
+    let dd = test.getDate();
+    let mm = test.getMonth() + 1; //January is 0!
 
+    let yyyy = test.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    let lavie = dd + '' + mm + '' + yyyy;;
+    return lavie
+  }
    addDays = () => {
      let result = new Date(this.reverse());
     result.setDate(result.getDate() + 7);
-    console.log(result)
      let test = result;
      let dd = test.getDate();
      let mm = test.getMonth() + 1; //January is 0!
@@ -44,15 +59,41 @@ export default class Availabilities extends React.Component {
      let lavie = dd + '' + mm + '' + yyyy;;
      return lavie
     }
+  
+  subDays = () => {
+    let result = new Date(this.reverse());
+    result.setDate(result.getDate() - 7);
+    let test = result;
+    let dd = test.getDate();
+    let mm = test.getMonth() + 1; //January is 0!
+
+    let yyyy = test.getFullYear();
+    if (dd < 10) {
+      dd = '0' + dd;
+    }
+    if (mm < 10) {
+      mm = '0' + mm;
+    }
+    let lavie = dd + '' + mm + '' + yyyy;;
+    return lavie
+  }
+
+  date = () => {
+    const today = new Date(),
+      date = today.getDate() + '' + (today.getMonth() + 1) + '' + today.getFullYear();
+    return date
+  }
  
-  handleNext = () => { this.props.history.push(`/teachers/2/availabilities/${this.addDays()}`)}
-  handlePrevious = () => { this.props.history.push("/teachers/2/availabilities/20122018")}
+  handleNext = () => { this.props.history.push(`/teachers/2/availabilities/${this.addDays()}`);this.caller()}
+  handlePrevious = () => { this.props.history.push(`/teachers/2/availabilities/${this.subDays()}`);this.caller()}
 
   render() {
-    // console.log(this.reverse());
     console.log(this.addDays())
+     console.log(this.todayDays())
+    
     return <div>
-      <button onClick={this.handlePrevious}>Previous Week</button>
+      {!(this.date() === this.props.match.params.date)?
+      <button onClick={this.handlePrevious}>Previous Week</button>:''}
         <h1>
           Week of{" "}
           {this.props.match.params.date
