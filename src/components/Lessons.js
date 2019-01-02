00345c5e1c4c03a94f19249d1884d0c955347ee3
anavarreteuk <a.navarrete.uk@gmail.com>
2019-01-02 12:49:02 +0000

    LessonTable, working with the first parameters

--- src/components/Lessons.js
+++ src/components/Lessons.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import "../App.css";
+import LessonTable from "./LessonTable";
 
 class Lessons extends Component {
 
@@ -16,9 +17,11 @@ componentDidMount () {
     return <div className="App">
         <h1>My lessons</h1>
       <h2>{this.props.studentLessons.map(value => value.date)}</h2>
+      <h2>{this.props.studentLessons.map(value => value.teacher_id)}</h2>
+
       <h3>{this.props.studentLessons.map(value=> value.availability).map(value => value.time)}</h3>
       <h3>{this.props.studentLessons.map(value => value.availability).map(value => value.duration)}</h3>
-
+      <LessonTable studentLessons={this.props.studentLessons}/> 
       </div>;
   }
 }
